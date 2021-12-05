97ab817d006208630af7e055c0dccb63fbac9809
Cuscì <79975885+cusci@users.noreply.github.com>
2021-12-05 16:52:37 +0100

    Update botstatus_teletips.py

--- botstatus_teletips.py
+++ botstatus_teletips.py
@@ -56,7 +56,7 @@ async def main_teletips():
                 xxx_teletips += f"\n\n✔️ Ultimo controllo: {last_update} ({TIME_ZONE})"
                 await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
                 print(f"Last checked on: {last_update}")                
-                await asyncio.sleep(21600)
+                await asyncio.sleep(86400)
                         
 app.run(main_teletips())
 
